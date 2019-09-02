a0f0e82d9b323870e621d446796ec6ac031452a6
Jay Wall <jaywall@gmail.com>
2019-09-02 12:38:19 -0400

    next rev

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TodoItem
 
-def todoView(request):
-    return render(request, 'todo.html')
+
+def todo_view(request):
+    all_todo_items = TodoItem.objects.all()
+    return render(request, 'todo.html',
+                  {'all_items': all_todo_items})
+
